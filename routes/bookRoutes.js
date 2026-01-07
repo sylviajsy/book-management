4021a1d3dafc0775f3248ba9cc76a67ea8c7fb0f
Siyi Ji <sylviajsyy@gmail.com>
2026-01-06 21:44:31 -0600

    Create function for JSON data

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -9,15 +9,34 @@ bookRoutes.get('/', (req, res) => {
 });
 
 // Get one book
-bookRoutes.get('/:isbn', async (req, res) => {
+bookRoutes.get('/:isbn', (req, res) => {
     const isbn = req.params.isbn;
     const book = books.find(b => b.isbn === isbn);
 
     if (!book) {
-        res.status(404).json({ error: 'Book not found' });
+        return res.status(404).json({ error: 'Book not found' });
     }
     
     res.json(book);
 });
+
+// Create a new book
+bookRoutes.post('/', (req, res) =>{
+    // get data from request body
+    const newBook = req.body;
+    const existingBook = books.find(b => b.isbn === newBook.isbn);
+
+    if (!newBook.isbn || !newBook.title || !newBook.author){
+        return res.status(404).json({ error: 'ISBN, Title and Author are required' });
+    } 
+
+    if (existingBook){
+        return res.status(404).json({ error: 'Book with this ISBN already exist'})
+    }
+
+    books.push(newBook);
+    res.json(books);
+}
+);
 
 export default bookRoutes;
