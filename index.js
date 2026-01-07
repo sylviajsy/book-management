97ef9db784940a73ec16adeaf82fceda522d45e0
Siyi Ji <sylviajsyy@gmail.com>
2026-01-07 17:32:04 -0600

    Connecting local db to server

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+// Using bodyParser
+app.use(express.urlencoded({ extended: true }));
 
 // Mount the book routes
 // Any request starting with '/books' will be handled by bookRoutes
 app.use('/books', bookRoutes);
+
+// Pool manages postgreSQL clients
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'nelson',
+    host: 'localhost',
+    database: 'bookManagement',
+    password: '',
+    port: 5432,
+})
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
