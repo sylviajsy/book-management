8d8367b1e7319cb5e3893c3e2e84ba5e739fab8e
Siyi Ji <sylviajsyy@gmail.com>
2026-01-06 22:49:12 -0600

    Delete function for JSON data

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -21,6 +21,12 @@ bookRoutes.get('/:isbn', (req, res) => {
 });
 
 // Create a new book
+// {
+//     "isbn": "06631324511",
+//     "title": "My First POST Book",
+//     "author": "Techtonica Student",
+//     "format": "Hardcover"
+// }
 bookRoutes.post('/', (req, res) =>{
     // get data from request body
     const newBook = req.body;
@@ -50,8 +56,24 @@ bookRoutes.put('/:isbn', (req, res) => {
         return res.status(404).json({ error: 'Book not found'})
     }
 
+    // Keep old data, update new data
+    // { "author": "Siyi Ji" }
     books[index] = { ...books[index], ...updates };
     res.json({ message: 'Book updated successfully', book: books[index] });
+})
+
+// Delete a book
+bookRoutes.delete('/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    const index = books.findIndex(b => b.isbn === isbn);
+
+    if (index === -1){
+        return res.status(404).json({ error: 'Book not found'})
+    }
+
+    // Remove one element at the specific index
+    books.splice(index, 1);
+    res.json({ message: 'Book deleted successfully', book: books });
 })
 
 export default bookRoutes;
