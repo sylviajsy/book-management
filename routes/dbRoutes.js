2ccbd9a50bcbf4d0831d9fd3da0612e1c52c83bf
Siyi Ji <sylviajsyy@gmail.com>
2026-01-08 00:15:58 -0600

    Get one book for db

--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -3,7 +3,7 @@ import db from '../db/index.js'
 
 const dbRoutes = Router();
 
-// Get all books data
+// Get all books
 dbRoutes.get('/', async (req, res) => {
     try {
         const bookTable = await db.query('SELECT * FROM books');
@@ -13,5 +13,22 @@ dbRoutes.get('/', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
+// Get one book
+dbRoutes.get('/:isbn', async (req, res) =>{
+    try {
+        const { isbn } = req.params;
+        const bookTable = await db.query('SELECT * FROM books WHERE isbn = $1', [isbn]);
+
+        if (bookTable.rows.length === 0){
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        
+        res.json(bookTable.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+})
 
 export default dbRoutes;
