f97d7159ceb2c6487b59e36eb187fe9872f80214
Siyi Ji <sylviajsyy@gmail.com>
2026-01-08 16:37:14 -0600

    Add pool info to .env file

--- db/index.js
+++ db/index.js
@@ -1,14 +1,17 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Pool management
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: 'nelson',
-    host: 'localhost',
-    database: 'bookManagement',
-    password: '',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DBNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 })
 
 export default {
