27aa5c156e32865bc71024b6432ba4738fb2674a
Siyi Ji <sylviajsyy@gmail.com>
2026-01-13 19:10:39 -0600

    Removing *.env in .gitignore

---
+++ db.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+
+  isbn VARCHAR(255) PRIMARY KEY,
+
+  title VARCHAR(255) NOT NULL,
+
+  author VARCHAR(255) NOT NULL,
+
+  format VARCHAR(255)
+
+);
+
+-- Insert is handled in db/dataSeed.js
