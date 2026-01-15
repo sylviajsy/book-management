2066c925622b4ad46163bf16b2eff765b50f5c69
Siyi Ji <sylviajsyy@gmail.com>
2026-01-14 22:42:19 -0600

    Adding backend URL to frontend

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import axios from 'axios';
 import './App.css'
 
+const API_URL = 'http://localhost:8080/dbBooks';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    const API_URL = 'http://localhost:8080/dbBooks';
+    
   )
 }
 
