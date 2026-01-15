85f7f7671faefe4054c33c7181e71253085b78fd
Siyi Ji <sylviajsyy@gmail.com>
2026-01-14 22:44:59 -0600

    use axios to fetch API

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -7,6 +7,15 @@ const API_URL = 'http://localhost:8080/dbBooks';
 function App() {
   const [count, setCount] = useState(0)
 
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get(API_URL);
+
+    } catch (error) {
+      console.error("error:", error);
+    }
+  }
+
   return (
     
   )
